41edee90f7adec248eedde3369522d2ba3d55b71
MartinPanelo <panelomartinn@hotmail.com>
2024-05-02 23:38:39 -0300

    implementacion de cambio de idioma

--- app/src/main/java/com/finallabtres/animalogistics/UI/auth/login/LoginActivity.java
+++ app/src/main/java/com/finallabtres/animalogistics/UI/auth/login/LoginActivity.java
@@ -1,16 +1,26 @@
 package com.finallabtres.animalogistics.UI.auth.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.os.LocaleListCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TableRow;
 
 import com.finallabtres.animalogistics.MainActivityViewModel;
 import com.finallabtres.animalogistics.R;
 import com.finallabtres.animalogistics.databinding.ActivityLoginBinding;
 import com.finallabtres.animalogistics.databinding.ActivityMainBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.button.MaterialButton;
+
+import java.util.Locale;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -60,6 +70,88 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+        /*-------------------------opcionesSheet-------------------------------*/
+
+        binding.FABOpciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(LoginActivity.this);
+                View view1 = LayoutInflater.from(LoginActivity.this).inflate(R.layout.opcionessheet, null);
+                bottomSheetDialog.setContentView(view1);
+                bottomSheetDialog.show();
+
+
+                TableRow MBEspañol = view1.findViewById(R.id.TREspañol);
+                TableRow MBEnglish = view1.findViewById(R.id.TREnglish);
+
+                MBEspañol.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        saveLocale("es");
+
+                        FijarIdioma("es");
+                    }
+                });
+
+
+                MBEnglish.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        saveLocale("en");
+                        FijarIdioma("en");
+                    }
+                });
+
+            }
+        });
+
+
+
+    }
+
+
+
+    // Método para obtener el idioma guardado en SharedPreferences
+    private String getSavedLocale() {
+        SharedPreferences preferences = getSharedPreferences("SP", Context.MODE_PRIVATE);
+        return preferences.getString("idioma", Locale.getDefault().getLanguage());
+    }
+
+    // Método para guardar el idioma en SharedPreferences
+    private void saveLocale(String languageCode) {
+        SharedPreferences preferences = getSharedPreferences("SP", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("idioma", languageCode);
+        editor.apply();
+    }
+
+
+    private void FijarIdioma(String Idioma){
+
+        AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags(Idioma));
+
+        //  LocaleListCompat localeListCompat =LocaleListCompat.forLanguageTags(Idioma);
+        //   setApplicationLocales(localeListCompat);
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
